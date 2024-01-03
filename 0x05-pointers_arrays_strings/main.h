8340690cb0a95be10979ca0293cd35e9caafed04
dzanduransfordselorm <selormransford2@gmail.com>
2024-01-02 20:36:33 -0800

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <unistd.h>
 
@@ -11,5 +14,5 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src)
 #endif
